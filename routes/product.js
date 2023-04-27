6ceaf14650dceea56b55764a5a4f8b9d4eddd50d
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-04-27 17:19:06 -0600

    Update the productController import to use it in the route statement

--- routes/product.js
+++ routes/product.js
@@ -4,9 +4,10 @@ const express = require('express');
 // Import express library to use the routes properties
 const router = express.Router();
 
+// Import the product controller to include its functionality
+const productController = require('../controllers/productController');
+
 // Defining the path equivalent to 'api/products' related to POST method
-router.post('/', () => {
-    console.log('Creating a new product');
-});
+router.post('/', productController.createProduct);
 
 module.exports = router;
