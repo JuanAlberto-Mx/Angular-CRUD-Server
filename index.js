dc46586f7bdff7de7dadf7c20cdd378f82fd8857
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-04-27 09:21:27 -0600

    Add index file with server creation and database connection functionalities

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello World from Index.js')
+// Import the express functionality
+const express = require('express');
+
+// Import the dbConnection functionalities
+const dbConnection = require('./config/dbconnection');
+
+// Creates the server
+const app = express();
+
+// Connect to the database
+dbConnection();
+
+// Defining a temporal path for the server
+app.get('/', (req, resp) => {
+    resp.send('Hello World');
+});
+
+// Enables the port 4000 to start the server and displays the corresponding message
+app.listen(4000, ()=>{
+    console.log("Server is running correctly");
+});
