c208e74a877ef3de3d80f22cca714c995f936702
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-05-04 17:51:03 -0600

    Update function to receive product objects

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,7 @@
 // Function to create a new product
 exports.createProduct = (req, resp) => {
-    console.log('Create Product Section')
+    console.log('Create Product Section');
+
+    // Printing the 'product' object received
+    console.log(req.body);
 };
