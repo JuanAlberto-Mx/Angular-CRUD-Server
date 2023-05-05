54173a47c9a3f2dfa95531d5c7b0341b5ebcb24d
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-05-05 08:30:25 -0600

    Update products insertion method to Compass DB

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,24 @@
-// Function to create a new product
-exports.createProduct = (req, resp) => {
-    console.log('Create Product Section');
+const Product = require("../models/Product");
 
-    // Printing the 'product' object received
-    console.log(req.body);
+// Function to create a new product
+exports.createProduct = async (req, resp) => {
+    try {
+        // Variable to set the product values
+        let product;
+
+        // Create the Product object with the values received in the request
+        product = new Product(req.body);
+
+        // Save the product in the database
+        await product.save();
+
+        // Send a message to the user with the Product values stored
+        resp.send(product);
+    }
+    catch(error){
+        console.log(error);
+
+        // Sending a response to the user
+        resp.status(500).send('An error ocurred during the product creation');
+    }
 };
