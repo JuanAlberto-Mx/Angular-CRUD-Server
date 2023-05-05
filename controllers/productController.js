74d0966bad80b1b17697d6e4deef89c81944dbfd
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-05-05 08:52:55 -0600

    Add the getProducts method to the controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -22,3 +22,19 @@ exports.createProduct = async (req, resp) => {
         resp.status(500).send('An error ocurred during the product creation');
     }
 };
+
+exports.getProducts = async (req, resp) => {
+    try {
+        // Obtain the product from the DB by using the find method
+        const product = await Product.find();
+
+        // Return the product in a JSON format
+        resp.json(product);
+    }
+    catch(error) {
+        console.log(error);
+
+        // Sending a response to the user
+        resp.status(500).send('It is not possible to obtain the products');
+    }
+};
